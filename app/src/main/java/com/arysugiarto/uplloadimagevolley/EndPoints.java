5c917221f77154507eeffe2c92f3864ebd3ed345
Ary Sugiarto <arysugiarto10@gmail.com>
2019-02-25 21:41:53 +0700

    Initial commit

---
+++ app/src/main/java/com/arysugiarto/uplloadimagevolley/EndPoints.java
@@ -0,0 +1,7 @@
+package com.arysugiarto.uplloadimagevolley;
+
+public class EndPoints {
+    public static final String ROOT_URL ="http://192.168.1.8/UploadImageAndroid/api.php?apicall=";
+    public static final String UPLOAD_URL = ROOT_URL + "uploadpic";
+    public static final String GET_PICS_URL = ROOT_URL + "getpics";
+}
